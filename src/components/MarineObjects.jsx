31e4bfb344bbf5928eaf83d1612c4510e89750b0
rohanshrma222 <hackerviruse04@gmail.com>
2026-03-10 18:52:56 +0530

    removed unused assets

--- src/components/MarineObjects.jsx
+++ src/components/MarineObjects.jsx
@@ -1,10 +1,10 @@
-import { useRef, useMemo, useEffect } from 'react'
-import { useFrame } from '@react-three/fiber'
-import { useGLTF, useAnimations } from '@react-three/drei'
-import * as THREE from 'three'
-import { MARINE_OBJECT_TYPES } from '../store'
+import { useRef, useEffect } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import * as THREE from "three";
+import { MARINE_OBJECT_TYPES } from "../store";
 
-function Clownfish({ color = '#ff6b35', ...props }) {
+function Clownfish({ color = "#ff6b35", ...props }) {
   return (
     <group {...props}>
       <mesh castShadow>
@@ -44,252 +44,55 @@ function Clownfish({ color = '#ff6b35', ...props }) {
         <meshPhongMaterial color="#ffffff" />
       </mesh>
     </group>
-  )
+  );
 }
 
-function Jellyfish({ color = '#ff69b4', ...props }) {
-  const groupRef = useRef()
-  const { scene, animations } = useGLTF('/jellyfish.glb')
-  const { actions, names } = useAnimations(animations, groupRef)
-
-  const clonedScene = useMemo(() => {
-    const cloned = scene.clone()
-    cloned.traverse((child) => {
-      if (child.isMesh && child.material) {
-        child.material = child.material.clone()
-        if (color) {
-          child.material.color = new THREE.Color(color)
-          child.material.emissive = new THREE.Color(color)
-          child.material.emissiveIntensity = 0.2
-        }
-      }
-    })
-    return cloned
-  }, [scene, color])
+function Jellyfish({ color = "#ff69b4", ...props }) {
+  const groupRef = useRef();
+  const { scene, animations } = useGLTF("/jellyfishR.glb");
 
   useEffect(() => {
-    console.log('Jellyfish animations:', names, actions)
+    scene.traverse((child) => {
+      if (child.isMesh && child.material) {
+        child.material = child.material.clone();
+        if (color) {
+          child.material.color = new THREE.Color(color);
+          child.material.emissive = new THREE.Color(color);
+          child.material.emissiveIntensity = 0.2;
+        }
+      }
+    });
+  }, [scene, color]);
+
+  const { actions, names } = useAnimations(animations, scene);
+
+  useEffect(() => {
     if (names.length > 0) {
       names.forEach((name) => {
-        const action = actions[name]
+        const action = actions[name];
         if (action) {
-          action.reset().fadeIn(0.5).play()
+          action.reset().play();
         }
-      })
+      });
     }
-  }, [actions, names])
-
-  useFrame((state) => {
-    if (groupRef.current) {
-      groupRef.current.position.y = Math.sin(state.clock.elapsedTime * 1.5) * 0.1
-    }
-  })
+  }, [actions, names]);
 
   return (
-    <group {...props}>
-      <primitive
-        object={clonedScene}
-        ref={groupRef}
-        scale={0.5}
-        position={[0, -0.5, 0]}
-      />
+    <group {...props} ref={groupRef}>
+      <primitive object={scene} scale={0.5} position={[0, -0.5, 0]} />
     </group>
-  )
-}
-
-function Shark({ color = '#4a5568', ...props }) {
-  return (
-    <group {...props}>
-      <mesh castShadow>
-        <sphereGeometry args={[0.5, 16, 12]} />
-        <meshPhongMaterial args={[color]} />
-      </mesh>
-      <mesh castShadow position={[-1.1, 0, 0]} rotation={[0, 0, -Math.PI / 2]}>
-        <coneGeometry args={[0.3, 0.7, 3]} />
-        <meshPhongMaterial args={[color]} />
-      </mesh>
-      <mesh castShadow position={[0.15, 0.45, 0]}>
-        <coneGeometry args={[0.25, 0.5, 3]} />
-        <meshPhongMaterial args={[color]} />
-      </mesh>
-      <mesh castShadow position={[-0.15, -0.15, 0.35]} rotation={[Math.PI / 4, 0, 0]}>
-        <coneGeometry args={[0.15, 0.4, 3]} />
-        <meshPhongMaterial args={[color]} />
-      </mesh>
-      <mesh castShadow position={[-0.15, -0.15, -0.35]} rotation={[-Math.PI / 4, 0, 0]}>
-        <coneGeometry args={[0.15, 0.4, 3]} />
-        <meshPhongMaterial args={[color]} />
-      </mesh>
-      <mesh position={[0.7, 0.12, 0.25]}>
-        <sphereGeometry args={[0.05, 8, 8]} />
-        <meshPhongMaterial color="#000000" />
-      </mesh>
-      <mesh position={[0.7, 0.12, -0.25]}>
-        <sphereGeometry args={[0.05, 8, 8]} />
-        <meshPhongMaterial color="#000000" />
-      </mesh>
-    </group>
-  )
-}
-
-function BrainCoral({ color = '#ed8936', ...props }) {
-  const geometry = useMemo(() => {
-    const geo = new THREE.SphereGeometry(0.4, 24, 16, 0, Math.PI * 2, 0, Math.PI / 2)
-    const positions = geo.attributes.position
-    for (let i = 0; i < positions.count; i++) {
-      const x = positions.getX(i)
-      const y = positions.getY(i)
-      const z = positions.getZ(i)
-      const noise = Math.sin(x * 8) * Math.cos(z * 8) * 0.06
-      positions.setX(i, x + noise * x * 0.5)
-      positions.setY(i, y + noise * 0.3)
-      positions.setZ(i, z + noise * z * 0.5)
-    }
-    geo.computeVertexNormals()
-    return geo
-  }, [])
-
-  return (
-    <group {...props}>
-      <mesh castShadow geometry={geometry}>
-        <meshPhongMaterial color={color} flatShading />
-      </mesh>
-      <mesh castShadow position={[0, -0.1, 0]}>
-        <cylinderGeometry args={[0.3, 0.4, 0.15, 16]} />
-        <meshPhongMaterial color={color} />
-      </mesh>
-    </group>
-  )
-}
-
-function Seaweed({ color = '#48bb68', ...props }) {
-  const bladesRef = useRef([])
-
-  useFrame((state) => {
-    bladesRef.current.forEach((blade, i) => {
-      if (blade) {
-        blade.rotation.z = Math.sin(state.clock.elapsedTime * 1.5 + i * 0.5) * 0.15
-        blade.rotation.x = Math.cos(state.clock.elapsedTime * 1.2 + i * 0.3) * 0.05
-      }
-    })
-  })
-
-  return (
-    <group {...props}>
-      {[...Array(6)].map((_, i) => (
-        <group
-          key={i}
-          position={[(Math.random() - 0.5) * 0.2, 0, (Math.random() - 0.5) * 0.2]}
-          ref={el => bladesRef.current[i] = el}
-        >
-          <mesh castShadow position={[0, 0.3, 0]}>
-            <planeGeometry args={[0.06, 0.6, 2, 6]} />
-            <meshPhongMaterial color={color} side={THREE.DoubleSide} />
-          </mesh>
-        </group>
-      ))}
-    </group>
-  )
-}
-
-function SeaFan({ color = '#fc8181', ...props }) {
-  return (
-    <group {...props}>
-      <mesh castShadow position={[0, 0.25, 0]}>
-        <cylinderGeometry args={[0.015, 0.03, 0.5, 6]} />
-        <meshPhongMaterial color={color} shininess={30} />
-      </mesh>
-      {[...Array(5)].map((_, i) => {
-        const angle = -0.6 + i * 0.3
-        return (
-          <group key={i} position={[0, 0.45, 0]} rotation={[angle, 0, 0]}>
-            <mesh castShadow position={[0, 0.15, 0]}>
-              <cylinderGeometry args={[0.01, 0.015, 0.3, 5]} />
-              <meshPhongMaterial color={color} />
-            </mesh>
-            <mesh castShadow position={[Math.sin(angle) * 0.15, 0.25, 0]} rotation={[angle * 0.5, 0, 0]}>
-              <cylinderGeometry args={[0.008, 0.012, 0.25, 5]} />
-              <meshPhongMaterial color={color} />
-            </mesh>
-          </group>
-        )
-      })}
-      <mesh castShadow position={[0, 0.05, 0]}>
-        <coneGeometry args={[0.08, 0.12, 8]} />
-        <meshPhongMaterial color={color} />
-      </mesh>
-    </group>
-  )
-}
-
-function Rock({ color = '#718096', ...props }) {
-  return (
-    <group {...props}>
-      {[
-        { pos: [0, 0.12, 0], r: 0.18 },
-        { pos: [0.12, 0.08, 0.08], r: 0.12 },
-        { pos: [-0.1, 0.1, -0.06], r: 0.1 },
-        { pos: [0.06, 0.16, -0.08], r: 0.08 },
-        { pos: [-0.04, 0.04, 0.12], r: 0.06 },
-      ].map((item, i) => (
-        <mesh key={i} castShadow position={item.pos} rotation={[i * 0.5, i * 0.7, 0]}>
-          <icosahedronGeometry args={[item.r, 0]} />
-          <meshPhongMaterial color={color} flatShading />
-        </mesh>
-      ))}
-    </group>
-  )
-}
-
-function SandPatch({ color = '#d69e2e', ...props }) {
-  return (
-    <group {...props}>
-      <mesh receiveShadow position={[0, 0.05, 0]}>
-        <cylinderGeometry args={[0.35, 0.45, 0.1, 24]} />
-        <meshPhongMaterial color={color} />
-      </mesh>
-    </group>
-  )
-}
-
-function Shell({ color = '#fbd38d', ...props }) {
-  return (
-    <group {...props}>
-      <mesh castShadow rotation={[0, 0, Math.PI / 4]}>
-        <sphereGeometry args={[0.12, 12, 8]} />
-        <meshPhongMaterial color={color} shininess={60} />
-      </mesh>
-      <mesh castShadow position={[-0.15, 0, 0]} rotation={[0, 0, -Math.PI / 2]}>
-        <coneGeometry args={[0.06, 0.18, 8]} />
-        <meshPhongMaterial color={color} shininess={60} />
-      </mesh>
-    </group>
-  )
+  );
 }
 
 export function MarineObject({ type, color, ...props }) {
-  const objectProps = { color, ...props }
+  const objectProps = { color, ...props };
 
   switch (type) {
     case MARINE_OBJECT_TYPES.FISH_CLOWNFISH:
-      return <Clownfish {...objectProps} />
+      return <Clownfish {...objectProps} />;
     case MARINE_OBJECT_TYPES.FISH_JELLYFISH:
-      return <Jellyfish {...objectProps} />
-    case MARINE_OBJECT_TYPES.FISH_SHARK:
-      return <Shark {...objectProps} />
-    case MARINE_OBJECT_TYPES.CORAL_BRAIN:
-      return <BrainCoral {...objectProps} />
-    case MARINE_OBJECT_TYPES.PLANT_SEAWEED:
-      return <Seaweed {...objectProps} />
-    case MARINE_OBJECT_TYPES.CORAL_FAN:
-      return <SeaFan {...objectProps} />
-    case MARINE_OBJECT_TYPES.TERRAIN_ROCK:
-      return <Rock {...objectProps} />
-    case MARINE_OBJECT_TYPES.TERRAIN_SAND:
-      return <SandPatch {...objectProps} />
-    case MARINE_OBJECT_TYPES.TERRAIN_SHELL:
-      return <Shell {...objectProps} />
+      return <Jellyfish {...objectProps} />;
     default:
-      return <Clownfish {...objectProps} />
+      return <Clownfish {...objectProps} />;
   }
 }
--- src/store.js
+++ src/store.js
@@ -1,84 +1,61 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 export const MARINE_OBJECT_TYPES = {
-  FISH_CLOWNFISH: 'fish-clownfish',
-  FISH_JELLYFISH: 'fish-jellyfish',
-  FISH_SHARK: 'fish-shark',
-  CORAL_BRAIN: 'coral-brain',
-  PLANT_SEAWEED: 'plant-seaweed',
-  CORAL_FAN: 'coral-fan',
-  TERRAIN_ROCK: 'terrain-rock',
-  TERRAIN_SAND: 'terrain-sand',
-  TERRAIN_SHELL: 'terrain-shell',
-}
+  FISH_CLOWNFISH: "fish-clownfish",
+  FISH_JELLYFISH: "fish-jellyfish",
+};
 
 export const OBJECT_CATEGORIES = {
   FISH: {
-    label: 'Fish',
-    types: [MARINE_OBJECT_TYPES.FISH_CLOWNFISH, MARINE_OBJECT_TYPES.FISH_JELLYFISH, MARINE_OBJECT_TYPES.FISH_SHARK],
+    label: "Fish",
+    types: [
+      MARINE_OBJECT_TYPES.FISH_CLOWNFISH,
+      MARINE_OBJECT_TYPES.FISH_JELLYFISH,
+    ],
   },
-  CORAL: {
-    label: 'Coral',
-    types: [MARINE_OBJECT_TYPES.CORAL_BRAIN, MARINE_OBJECT_TYPES.CORAL_FAN],
-  },
-  PLANTS: {
-    label: 'Plants',
-    types: [MARINE_OBJECT_TYPES.PLANT_SEAWEED],
-  },
-  TERRAIN: {
-    label: 'Terrain',
-    types: [MARINE_OBJECT_TYPES.TERRAIN_ROCK, MARINE_OBJECT_TYPES.TERRAIN_SAND, MARINE_OBJECT_TYPES.TERRAIN_SHELL],
-  },
-}
+};
 
 export const BEHAVIORS = {
-  STATIC: 'static',
-  SWIM_HORIZONTAL: 'swim-horizontal',
-  SWIM_VERTICAL: 'swim-vertical',
-  FLOAT: 'float',
-  SWAY: 'sway',
-}
+  STATIC: "static",
+  SWIM_HORIZONTAL: "swim-horizontal",
+  SWIM_VERTICAL: "swim-vertical",
+  FLOAT: "float",
+  SWAY: "sway",
+};
 
 export const BEHAVIOR_OPTIONS = [
-  { value: BEHAVIORS.STATIC, label: 'Static' },
-  { value: BEHAVIORS.SWIM_HORIZONTAL, label: 'Swim Horizontal' },
-  { value: BEHAVIORS.SWIM_VERTICAL, label: 'Swim Vertical' },
-  { value: BEHAVIORS.FLOAT, label: 'Float' },
-  { value: BEHAVIORS.SWAY, label: 'Sway' },
-]
+  { value: BEHAVIORS.STATIC, label: "Static" },
+  { value: BEHAVIORS.SWIM_HORIZONTAL, label: "Swim Horizontal" },
+  { value: BEHAVIORS.SWIM_VERTICAL, label: "Swim Vertical" },
+  { value: BEHAVIORS.FLOAT, label: "Float" },
+  { value: BEHAVIORS.SWAY, label: "Sway" },
+];
 
 export const DEFAULT_COLORS = {
-  [MARINE_OBJECT_TYPES.FISH_CLOWNFISH]: '#ff6b35',
-  [MARINE_OBJECT_TYPES.FISH_JELLYFISH]: '#ff69b4',
-  [MARINE_OBJECT_TYPES.FISH_SHARK]: '#4a5568',
-  [MARINE_OBJECT_TYPES.CORAL_BRAIN]: '#ed8936',
-  [MARINE_OBJECT_TYPES.PLANT_SEAWEED]: '#48bb68',
-  [MARINE_OBJECT_TYPES.CORAL_FAN]: '#fc8181',
-  [MARINE_OBJECT_TYPES.TERRAIN_ROCK]: '#718096',
-  [MARINE_OBJECT_TYPES.TERRAIN_SAND]: '#d69e2e',
-  [MARINE_OBJECT_TYPES.TERRAIN_SHELL]: '#fbd38d',
-}
+  [MARINE_OBJECT_TYPES.FISH_CLOWNFISH]: "#ff6b35",
+  [MARINE_OBJECT_TYPES.FISH_JELLYFISH]: "#ff69b4",
+};
 
-const MAX_OBJECTS = 50
+const MAX_OBJECTS = 50;
 
-let objectIdCounter = 0
+let objectIdCounter = 0;
 
 export const useStore = create((set, get) => ({
   objects: [],
   selectedId: null,
   isLoading: false,
   showGrid: true,
-  
+
   addObject: (type, position = null) => {
-    const { objects } = get()
+    const { objects } = get();
     if (objects.length >= MAX_OBJECTS) {
-      console.warn(`Maximum object limit (${MAX_OBJECTS}) reached`)
-      return null
+      console.warn(`Maximum object limit (${MAX_OBJECTS}) reached`);
+      return null;
     }
-    
-    const id = ++objectIdCounter
-    const defaultColor = DEFAULT_COLORS[type] || '#888888'
-    
+
+    const id = ++objectIdCounter;
+    const defaultColor = DEFAULT_COLORS[type] || "#888888";
+
     const newObject = {
       id,
       type,
@@ -92,90 +69,94 @@ export const useStore = create((set, get) => ({
       color: defaultColor,
       behavior: BEHAVIORS.STATIC,
       speed: 1,
-    }
-    
-    set({ objects: [...objects, newObject] })
-    set({ selectedId: id })
-    
-    return newObject
+    };
+
+    set({ objects: [...objects, newObject] });
+    set({ selectedId: id });
+
+    return newObject;
   },
-  
+
   removeObject: (id) => {
-    const { objects, selectedId } = get()
+    const { objects, selectedId } = get();
     set({
-      objects: objects.filter(obj => obj.id !== id),
+      objects: objects.filter((obj) => obj.id !== id),
       selectedId: selectedId === id ? null : selectedId,
-    })
+    });
   },
-  
+
   clearAll: () => {
-    set({ objects: [], selectedId: null })
+    set({ objects: [], selectedId: null });
   },
-  
+
   selectObject: (id) => {
-    set({ selectedId: id })
+    set({ selectedId: id });
   },
-  
+
   deselectObject: () => {
-    set({ selectedId: null })
+    set({ selectedId: null });
   },
-  
+
   updateObject: (id, updates) => {
-    const { objects } = get()
+    const { objects } = get();
     set({
-      objects: objects.map(obj => 
-        obj.id === id ? { ...obj, ...updates } : obj
-      )
-    })
+      objects: objects.map((obj) =>
+        obj.id === id ? { ...obj, ...updates } : obj,
+      ),
+    });
   },
-  
+
   updatePosition: (id, axis, value) => {
-    const { objects } = get()
+    const { objects } = get();
     set({
-      objects: objects.map(obj => 
-        obj.id === id ? { ...obj, position: { ...obj.position, [axis]: value } } : obj
-      )
-    })
+      objects: objects.map((obj) =>
+        obj.id === id
+          ? { ...obj, position: { ...obj.position, [axis]: value } }
+          : obj,
+      ),
+    });
   },
-  
+
   updateRotation: (id, axis, value) => {
-    const { objects } = get()
+    const { objects } = get();
     set({
-      objects: objects.map(obj => 
-        obj.id === id ? { ...obj, rotation: { ...obj.rotation, [axis]: value } } : obj
-      )
-    })
+      objects: objects.map((obj) =>
+        obj.id === id
+          ? { ...obj, rotation: { ...obj.rotation, [axis]: value } }
+          : obj,
+      ),
+    });
   },
-  
+
   toggleGrid: () => {
-    set(state => ({ showGrid: !state.showGrid }))
+    set((state) => ({ showGrid: !state.showGrid }));
   },
-  
+
   getSelectedObject: () => {
-    const { objects, selectedId } = get()
-    return objects.find(obj => obj.id === selectedId) || null
+    const { objects, selectedId } = get();
+    return objects.find((obj) => obj.id === selectedId) || null;
   },
-  
+
   exportScene: () => {
-    const { objects } = get()
+    const { objects } = get();
     return {
       objects: objects.map(({ id, ...rest }) => rest),
       metadata: {
         exportedAt: new Date().toISOString(),
         objectCount: objects.length,
       },
-    }
+    };
   },
-  
+
   importScene: (data) => {
-    if (!data?.objects) return false
-    
+    if (!data?.objects) return false;
+
     const importedObjects = data.objects.map((obj, index) => ({
       ...obj,
       id: ++objectIdCounter,
-    }))
-    
-    set({ objects: importedObjects, selectedId: null })
-    return true
+    }));
+
+    set({ objects: importedObjects, selectedId: null });
+    return true;
   },
-}))
+}));
